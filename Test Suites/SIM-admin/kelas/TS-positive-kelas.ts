5ac1f982a3169448b68249074da3d1b052683a1e
telkomdev-rahmatprastyo <rahmatprastyo88@gmail.com>
2021-02-22 20:12:37 +0700

    update

---
+++ Test Suites/SIM-admin/kelas/TS-positive-kelas.ts
@@ -0,0 +1,92 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS-positive-kelas</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>85873a96-8749-47b4-98e7-d11012d7ccd1</testSuiteGuid>
+   <testCaseLink>
+      <guid>75c95341-ff80-46d7-8ba2-513c141b271f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CustomKeyword/Open Browser</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>4d69f038-091a-4756-beaa-a71c268725d3</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CustomKeyword/Navigate to URL SIM</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>170b2a13-9734-4e19-b15d-758866197254</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/SIM-admin/login/positive cases/TC001-Pastikan admin bisa login</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>b2ec5de0-110a-4d8e-8125-4bdcee07185f</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>98958674-7371-4675-a36b-07727b065d71</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>bd2de256-6ab8-4f60-964a-2da52b988474</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/SIM-admin/kelas/positive cases/TC001-Pastikan user dapat menambahkan data kelas</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>4369ae6d-5325-4161-b9f0-e009168100d2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/SIM-admin/kelas/positive cases/TC002-Pastikan user dapat menampahkan data dengan import data</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>8edf8ad0-7f13-416c-9473-4b2ad83a97de</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/SIM-admin/kelas/positive cases/TC003-Pastikan user dapat menggunakan fitur search kelas</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>e1c600cb-a4cb-42f8-af36-4fff68cba86a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/SIM-admin/kelas/positive cases/TC004-Pastikan user bisa melihat detail kelas</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>64e5b279-feb9-47ff-9856-24041cc6d7e3</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/SIM-admin/kelas/positive cases/TC005-Pastikan user bisa edit data kelas</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>e2ec7a36-3581-4336-a243-f3e1f62f9796</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/SIM-admin/kelas/positive cases/TC006-Pastikan user bisa menghapus data kelas</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b4b31749-3d4d-46de-a2d6-7722d9cb975d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/SIM-admin/kelas/positive cases/TC007-Pastikan user dapat unduh data kelas</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>3dad136d-8d4c-4b12-a27d-02af6ce5b7fa</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/SIM-admin/kelas/positive cases/TC008-Pastikan user dapat print data kelas</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
