7a9895a88d94cc78d8747304ca7c0b418aada194
telkomdev-rahmatprastyo <rahmatprastyo88@gmail.com>
2021-02-22 18:54:20 +0700

    update tc dan ts admin data kelas -masih kurang pagination

---
+++ Test Suites/TSC/develop/TSC-SIM-admin-kelas.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteCollectionEntity>
+   <description></description>
+   <name>TSC-SIM-admin-kelas</name>
+   <tag></tag>
+   <executionMode>SEQUENTIAL</executionMode>
+   <maxConcurrentInstances>8</maxConcurrentInstances>
+   <testSuiteRunConfigurations>
+      <TestSuiteRunConfiguration>
+         <configuration>
+            <groupName>Web Desktop</groupName>
+            <profileName>develop</profileName>
+            <runConfigurationId>Chrome</runConfigurationId>
+         </configuration>
+         <runEnabled>true</runEnabled>
+         <testSuiteEntity>Test Suites/SIM-admin/kelas/TC-positive-kelas</testSuiteEntity>
+      </TestSuiteRunConfiguration>
+   </testSuiteRunConfigurations>
+</TestSuiteCollectionEntity>
