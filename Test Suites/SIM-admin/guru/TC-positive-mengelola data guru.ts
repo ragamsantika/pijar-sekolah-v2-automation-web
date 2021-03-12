985b26e94a5aa5ccc6a694c99663f1bf3028af08
Rahmat Prastyo <rahmatprastyo88@gmail.com>
2021-03-12 14:40:53 +0700

    update tc

--- Test Suites/SIM-admin/guru/TC-positive-mengelola data guru.ts
+++ Test Suites/SIM-admin/guru/TC-positive-mengelola data guru.ts
@@ -54,30 +54,6 @@
       <testCaseId>Test Cases/SIM-admin/data guru/positive cases/Pastikan user dapat menambahkan data guru</testCaseId>
    </testCaseLink>
    <testCaseLink>
-      <guid>89f14069-ac15-48f2-bdaf-c36671a1d7c6</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/SIM-admin/data guru/positive cases/Pastikan user dapat menggunakan fitur search</testCaseId>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>a720788e-d39f-4109-951b-c1c40c721a78</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/SIM-admin/data guru/positive cases/Pastikan user dapat mengedit data guru</testCaseId>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>4c25d134-d2b4-4dc6-ad10-b86b914542a6</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/SIM-admin/data guru/positive cases/Pastikan user dapat mengganti password guru</testCaseId>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>aaba5544-8a43-485d-82d4-31a46c2d5f2d</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/SIM-admin/data guru/positive cases/Pastikan user dapat menghapus data guru</testCaseId>
-   </testCaseLink>
-   <testCaseLink>
       <guid>33bfdd69-a45a-44af-8715-2d0e96c03b81</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
